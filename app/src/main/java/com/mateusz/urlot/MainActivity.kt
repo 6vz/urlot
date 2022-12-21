741946c01d3b46be52cdeeec381ccfae78156c72
Matt <mateuszbaranowski@protonmail.com>
2022-12-21 21:48:03 +0100

    hi mom!

--- app/src/main/java/com/mateusz/urlot/MainActivity.kt
+++ app/src/main/java/com/mateusz/urlot/MainActivity.kt
@@ -20,6 +20,10 @@ class MainActivity : AppCompatActivity() {
 
         val startButton = findViewById<Button>(R.id.startPlan)
         val dayCounter = findViewById<TextView>(R.id.dayCounter)
+        val fromPlaceholder = findViewById<TextView>(R.id.fromPlaceholder)
+        val tillPlaceholder = findViewById<TextView>(R.id.tillPlaceholder)
+        val pricePlaceholder = findViewById<TextView>(R.id.pricePlaceholder)
+
 
         startButton.setOnClickListener {
             val drp = MaterialDatePicker.Builder.dateRangePicker()
@@ -48,7 +52,10 @@ class MainActivity : AppCompatActivity() {
                 val endDate = drp.selection?.second
                 // check if start date already passed
                 if (startDate!! < System.currentTimeMillis()) {
-                    dayCounter.text = "0 dni"
+                    dayCounter.text = "- dni"
+                    fromPlaceholder.text = ""
+                    tillPlaceholder.text = ""
+
                     Snackbar.make(
                         findViewById(R.id.root),
                         "Nie można zaplanować podróży w przeszłości. Co ty myślisz, że my cudotwórcy?",
@@ -56,7 +63,9 @@ class MainActivity : AppCompatActivity() {
                     ).show()
                 // else if from start or end date is more than 2 years away, cancel and send snackbar
                 } else if (startDate > System.currentTimeMillis() + 63113904000 || endDate!! > System.currentTimeMillis() + 63113904000) {
-                    dayCounter.text = "0 dni"
+                    dayCounter.text = "- dni"
+                    fromPlaceholder.text = ""
+                    tillPlaceholder.text = ""
                     Snackbar.make(
                         findViewById(R.id.root),
                         "Nie możesz zaplanować podróży na więcej niż 2 lata w przyszłość",
@@ -71,6 +80,9 @@ class MainActivity : AppCompatActivity() {
                     dayCounter.text = "${days.toString()} dni"
                     val startLocalDate = Instant.ofEpochMilli(startDate).atZone(ZoneId.of("Europe/Warsaw")).toLocalDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
                     val endLocalDate = Instant.ofEpochMilli(endDate!!).atZone(ZoneId.of("Europe/Warsaw")).toLocalDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+                    fromPlaceholder.text = "Rozpoczęcie podróży w dniu $startLocalDate"
+                    tillPlaceholder.text = "Zakończenie podróży w dniu $endLocalDate"
+                    pricePlaceholder.text = "Przewidywana Cena: ${days * 529} zł"
                     Snackbar.make(
                         findViewById(R.id.root),
                         "Poczekaj chwile, nasze gnomy właśnie obliczają twoją podróż w terminie od ${startLocalDate} do ${endLocalDate}",
